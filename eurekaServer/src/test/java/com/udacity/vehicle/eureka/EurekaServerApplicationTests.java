65fbb5af9b21aeb6656e490f638baf71c58c52f3
Arthus Ni <arthus8ni@gmail.com>
2021-08-31 19:05:40 +0200

    eureka server added

---
+++ eurekaServer/src/test/java/com/udacity/vehicle/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.udacity.vehicle.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
--- vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Launches a Spring Boot application for the Vehicles API,
  * initializes the car manufacturers in the database,
@@ -18,6 +20,7 @@ import org.springframework.web.reactive.function.client.WebClient;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class VehiclesApiApplication {
 
     public static void main(String[] args) {
